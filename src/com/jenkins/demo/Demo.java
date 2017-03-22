f62409833471d0d3f61f866e984885396b17dc98
Administrator <Administrator@BLCL-HP-56.blazeclan.local>
2017-03-22 14:31:30 +0530

    New created repository

---
+++ src/com/jenkins/demo/Demo.java
@@ -0,0 +1,15 @@
+package com.jenkins.demo;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+	
+	@Test
+	public void jDemo(){
+		
+		System.out.println("welcome to jenkins");
+		
+		
+	}
+
+}
